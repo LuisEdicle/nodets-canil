f6bc585b9051f1e32600f5ef82daf7796847ae78
luis lima <luislima@MacBook-Air-de-luis.local>
2025-03-30 08:12:15 -0300

    Projeto Final 1.0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', PageController.home);
 router.get('/dogs', PageController.dogs);
 router.get('/cats', PageController.cats);
-router.get('/fisher', PageController.fisher);
+router.get('/fish', PageController.fish);
 
 router.get('/search', SearchController.search);
 
